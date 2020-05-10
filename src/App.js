09ca6d48053447102992fb092263ab21ed1b507f
Jay-R Joseph Gabunada <jayrjoseph@dnamicro.com>
2020-05-11 07:29:17 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import './App.css'
 import { Stage, Layer, Rect, Text, Image } from 'react-konva'
 import Konva from 'konva'
-import { ReactComponent as Pattern } from './drink.svg'
+import { ReactComponent as Pattern } from './i-like-food.svg'
 import Logo from './logo.svg'
 import useImage from 'use-image'
 import ReactDOMServer from 'react-dom/server'
@@ -14,18 +14,21 @@ import htmlToImage from 'html-to-image'
 import Upload from './components/Upload'
 import { v4 as uuid } from 'uuid'
 
+function generateRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
 
 function ModifiedSVG(props) {
   const { color, setSvg, setPatternColors } = props
 
   const svgRef = useRef()
 
-  const setFill = node => color =>  {
-    node.childNodes.forEach(node => {
+  const setFill = (node) => (color) => {
+    node.childNodes.forEach((node) => {
       Object.entries(color).forEach(([key, value]) => {
-        if(key === node.dataset.swatchId) {
+        if (key === node.dataset.swatchId) {
           node.style.fill = value
-          if(node.childNodes.length > 0) {
+          if (node.childNodes.length > 0) {
             setFill(node)(color)
           }
         }
@@ -33,17 +36,13 @@ function ModifiedSVG(props) {
     })
   }
 
-  function generateRandomColor() {
-    return `#${Math.floor(Math.random()*16777215).toString(16)}`
-  }
-
   function generateSwatches(node) {
-    node.childNodes.forEach(node => {
-      if(node.style.fill !== null) {
-        const id =  uuid()
+    node.childNodes.forEach((node) => {
+      if (node.style.fill !== null) {
+        const id = uuid()
         node.dataset.swatchId = id
         console.log(node)
-        setPatternColors(state => {
+        setPatternColors((state) => {
           return {
             ...state,
             [id]: generateRandomColor()
@@ -76,9 +75,9 @@ function ModifiedSVG(props) {
 
 function App() {
   const [patternColors, setPatternColors] = useState({
-    background: 'orange'
+    background: generateRandomColor()
   })
-  console.log("App -> patternColors", patternColors)
+  console.log('App -> patternColors', patternColors)
 
   const [svg, setSvg] = useState()
   const [pic, setPic] = useState()
@@ -110,26 +109,34 @@ function App() {
 
   return (
     <div className='App'>
-      <Stage width={window.innerWidth / 2} height={window.innerHeight / 3}>
+      <Stage width={window.innerWidth / 1.5} height={window.innerHeight / 1.5}>
         <Layer>
           <Rect
-            width={window.innerWidth / 2}
-            height={window.innerHeight / 3}
+            width={window.innerWidth / 1.5}
+            height={window.innerHeight / 1.5}
             fillPatternImage={image}
           />
         </Layer>
       </Stage>
-      {Object.entries(patternColors).map(([key, value]) => {
-        return (
-          <Swatch
-            key={key}
-            ground={key}
-            color={value}
-            onChangeHandler={onChangeHandler}
-          />
-        )
-      })}
-      <ModifiedSVG setSvg={setSvg} color={patternColors} setPatternColors={setPatternColors} />
+      <div style={{ display: 'flex' }}>
+        {Object.entries(patternColors).map(([key, value]) => {
+          return (
+            <Swatch
+              key={key}
+              ground={key}
+              color={value}
+              onChangeHandler={onChangeHandler}
+            />
+          )
+        })}
+      </div>
+      <div style={{ opacity: '0',  position: 'absolute', zIndex: '-100' }}>
+        <ModifiedSVG
+          setSvg={setSvg}
+          color={patternColors}
+          setPatternColors={setPatternColors}
+        />
+      </div>
     </div>
   )
 }
--- src/components/Swatch.js
+++ src/components/Swatch.js
@@ -1,19 +1,38 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { ChromePicker } from 'react-color'
 
 export default function Swatch(props) {
   const { color, onChangeHandler, ground } = props
   const [displayColorPicker, setDisplayColorPicker] = useState(false)
+
   const handleDisplayColorPicker = () => {
     setDisplayColorPicker(!displayColorPicker)
   }
 
+  const swatchRef = useRef()
+
+  useEffect(() => {
+    document.addEventListener('click', (e) => {
+      if (swatchRef.current.contains(e.target)) return
+      setDisplayColorPicker(false)
+    })
+
+    return () => document.removeEventListener('click')
+  }, [])
 
   return (
-    <div>
-      {displayColorPicker && <ChromePicker color={color} onChange={onChangeHandler(ground)} />}
+    <div ref={swatchRef}>
+      {displayColorPicker && (
+        <ChromePicker color={color} onChange={onChangeHandler(ground)} />
+      )}
       <svg width='40' height='40'>
-        <circle cx='20' cy='20' r='20' fill={color} onClick={handleDisplayColorPicker} />
+        <circle
+          cx='20'
+          cy='20'
+          r='20'
+          fill={color}
+          onClick={handleDisplayColorPicker}
+        />
       </svg>
     </div>
   )
