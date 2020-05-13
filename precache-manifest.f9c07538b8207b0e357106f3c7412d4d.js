4f942555fb5f3b53292ca45487c1575c5cf6e8cd
Jay-R Joseph Gabunada <jayrjoseph@dnamicro.com>
2020-05-14 06:15:40 +0800

    Updates

---
+++ precache-manifest.f9c07538b8207b0e357106f3c7412d4d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9b9c480772f9623867e1c208fb6278",
+    "url": "/svg-practice/index.html"
+  },
+  {
+    "revision": "4bb241f8c4c9b249b5c5",
+    "url": "/svg-practice/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7aad697790690c52e85b",
+    "url": "/svg-practice/static/js/2.db622e22.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/svg-practice/static/js/2.db622e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb241f8c4c9b249b5c5",
+    "url": "/svg-practice/static/js/main.ea6692ff.chunk.js"
+  },
+  {
+    "revision": "84ef91cab0baa2c85c5f",
+    "url": "/svg-practice/static/js/runtime-main.268af8d7.js"
+  },
+  {
+    "revision": "ea49168ba534d448e182b30ae3ab624e",
+    "url": "/svg-practice/static/media/art-10.ea49168b.jpg"
+  },
+  {
+    "revision": "e1aa5e349df330e41394109b8df55abf",
+    "url": "/svg-practice/static/media/front.e1aa5e34.png"
+  },
+  {
+    "revision": "5994ea68033462d3508a94561c09dc34",
+    "url": "/svg-practice/static/media/i-like-food.5994ea68.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/svg-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
