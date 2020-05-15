719134dafafa58ad71a0dd3dc8425edeb5e112fd
Jay-R Joseph Gabunada <jayrjoseph@dnamicro.com>
2020-05-15 11:26:01 +0800

    partially done with outline

--- README.md
+++ README.md
@@ -4,6 +4,28 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `docker-compose up --build`
+
+Builds the image and run the container. <br />
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `docker-compose up`
+
+Runs the container. <br />
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `docker-compose build`
+
+Builds the image.
+
 ### `yarn start`
 
 Runs the app in the development mode.<br />
--- src/components/ShirtComponent.js
+++ src/components/ShirtComponent.js
@@ -11,13 +11,16 @@ import { Border } from '../utils/Border'
 import useImage from 'use-image'
 
 const drawOutline = ctx => points =>  {
-  ctx.strokeStyle="red";
-  ctx.lineWidth=2;
+
+  // ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+  // ctx.strokeStyle="red";
+  // ctx.lineWidth=2;
   ctx.beginPath();
   ctx.moveTo(points[0][0],points[0][1]);
   for(var i=1;i<points.length;i++){
-      var point=points[i];
-      ctx.lineTo(point[0],point[1]);
+    var point=points[i];
+    ctx.lineTo(point[0],point[1]);
+    // debugger
   }
   ctx.closePath();
   ctx.stroke();
@@ -29,7 +32,7 @@ export default function PartsImage({ base }) {
   shirt.src = part
   const back = new Image()
   back.src = onepiece
-  const [image] = useImage(part, 'Anonymous')
+  const [image] = useImage(part, 'anonymous')
 
   const imageRef = useRef()
 
@@ -37,17 +40,27 @@ export default function PartsImage({ base }) {
   useEffect(() => {
     if (image) {
       const shape = imageRef.current
-      console.log("PartsImage -> shape", shape)
 
-      // const ctx = shape.getContext()
-      // const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height).data
-      // console.log("PartsImage -> imgData", imgData)
-      // // Border.call(shape, imgData)
-      // const points = d3_contour(defineTransparent(imgData)(ctx.canvas.width))
-      // console.log("PartsImage -> points", points)
+      const ctx = shape.getContext()
 
+      const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+      console.log("PartsImage -> imgData", imgData)
+      const points = d3_contour(defineTransparent(imgData.data)(ctx.canvas.width))
+      console.log("PartsImage -> points", points)
+      const layer = shape.getLayer()
 
-      // redraw(ctx)(points)
+      var outline = new Konva.Shape({
+        sceneFunc: function (context, shape) {
+          drawOutline(context)(points)
+          // (!) Konva specific method, it is very important
+          context.fillStrokeShape(shape);
+        },
+        draggable: true,
+        stroke: 'red',
+        strokeWidth: 4,
+      });
+      layer.add(outline)
+
 
       // you many need to reapply cache on some props changes like shadow, stroke, etc.
       shape.cache();
@@ -66,14 +79,14 @@ export default function PartsImage({ base }) {
         draggable
         sceneFunc={(ctx, shape) => {
 
-          ctx.drawImage(shirt, 0,0)
+          ctx.drawImage(shirt, 0, 0, ctx.canvas.width / 2, ctx.canvas.height / 2 )
 
-          const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-          const points = d3_contour(defineTransparent(imgData.data)(ctx.canvas.width))
-          drawOutline(ctx)(points)
+          // const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+          // const points = d3_contour(defineTransparent(imgData.data)(ctx.canvas.width))
+          // drawOutline(ctx)(points)
 
-          // ctx.globalCompositeOperation = 'source-in'
-          // ctx.drawImage(back, -shape.x(), -shape.y())
+          ctx.globalCompositeOperation = 'source-in'
+          ctx.drawImage(back, -shape.x(), -shape.y())
 
         }}
         hitFunc={(ctx, image) => {
