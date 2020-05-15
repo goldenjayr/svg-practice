c27f91a2eb10b2cd3df0324b4dd92c646980a858
Jay-R Joseph Gabunada <jayrjoseph@dnamicro.com>
2020-05-15 13:26:46 +0800

    hapit na jud

--- src/components/ShirtComponent.js
+++ src/components/ShirtComponent.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react'
 import { Shape, Image as KonvaImage } from 'react-konva'
-import Konva from 'konva';
+import Konva from 'konva'
 import part from '../lightning.png'
 // import part from '../tshirt-round-neck-men/mesh/front.png'
 import onepiece from '../onepiece.jpg'
@@ -10,21 +10,22 @@ import { debounce, throttle } from 'lodash'
 import { Border } from '../utils/Border'
 import useImage from 'use-image'
 
-const drawOutline = ctx => points =>  {
-
+const drawOutline = (ctx) => (points) => {
   // ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
   // ctx.strokeStyle="red";
   // ctx.lineWidth=2;
-  ctx.beginPath();
-  ctx.moveTo(points[0][0],points[0][1]);
-  for(var i=1;i<points.length;i++){
-    var point=points[i];
-    ctx.lineTo(point[0],point[1]);
+
+  const xOffset = 23
+  const yOffset = 12
+  ctx.beginPath()
+  ctx.moveTo(points[0][0] - xOffset, points[0][1] - yOffset)
+  for (var i = 1; i < points.length; i++) {
+    var point = points[i]
+    ctx.lineTo(point[0] - xOffset, point[1] - yOffset)
     // debugger
   }
-  ctx.closePath();
-  ctx.stroke();
-
+  ctx.closePath()
+  ctx.stroke()
 }
 
 export default function PartsImage({ base }) {
@@ -36,34 +37,40 @@ export default function PartsImage({ base }) {
 
   const imageRef = useRef()
 
-
   useEffect(() => {
     if (image) {
       const shape = imageRef.current
 
       const ctx = shape.getContext()
 
-      const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-      console.log("PartsImage -> imgData", imgData)
-      const points = d3_contour(defineTransparent(imgData.data)(ctx.canvas.width))
-      console.log("PartsImage -> points", points)
+      const imgData = ctx.getImageData(
+        0,
+        0,
+        ctx.canvas.width,
+        ctx.canvas.height
+      )
+      console.log('PartsImage -> ctx.canvas.width', ctx.canvas.width)
+      const points = d3_contour(
+        defineTransparent(imgData.data)(ctx.canvas.width)
+      )
       const layer = shape.getLayer()
 
       var outline = new Konva.Shape({
+        // x: -23,
+        // y: -12,
         sceneFunc: function (context, shape) {
           drawOutline(context)(points)
           // (!) Konva specific method, it is very important
-          context.fillStrokeShape(shape);
+          context.fillStrokeShape(shape)
         },
         draggable: true,
         stroke: 'red',
-        strokeWidth: 4,
-      });
+        strokeWidth: 4
+      })
       layer.add(outline)
 
-
       // you many need to reapply cache on some props changes like shadow, stroke, etc.
-      shape.cache();
+      shape.cache()
 
       // since this update is not handled by "react-konva" and we are using Konva methods directly
       // we have to redraw layer manually
@@ -71,35 +78,31 @@ export default function PartsImage({ base }) {
     }
   }, [image])
 
-
-
   return (
-      <Shape
-        ref={imageRef}
-        draggable
-        sceneFunc={(ctx, shape) => {
+    <Shape
+      ref={imageRef}
+      draggable
+      sceneFunc={(ctx, shape) => {
+        ctx.drawImage(shirt, 0, 0, ctx.canvas.width / 2, ctx.canvas.height / 2)
 
-          ctx.drawImage(shirt, 0, 0, ctx.canvas.width / 2, ctx.canvas.height / 2 )
+        // const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+        // const points = d3_contour(defineTransparent(imgData.data)(ctx.canvas.width))
+        // drawOutline(ctx)(points)
 
-          // const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-          // const points = d3_contour(defineTransparent(imgData.data)(ctx.canvas.width))
-          // drawOutline(ctx)(points)
-
-          ctx.globalCompositeOperation = 'source-in'
-          ctx.drawImage(back, -shape.x(), -shape.y())
-
-        }}
-        hitFunc={(ctx, image) => {
-          ctx.rect(0, 0, shirt.width, shirt.height)
-          ctx.fillStrokeShape(image)
-        }}
-      />
-      // <KonvaImage
-      //   image={image}
-      //   ref={imageRef}
-      //   filters={[Konva.Filters.Blur]}
-      //   blurRadius={10}
-      //   draggable
-      // />
+        ctx.globalCompositeOperation = 'source-in'
+        ctx.drawImage(back, -shape.x(), -shape.y())
+      }}
+      hitFunc={(ctx, image) => {
+        ctx.rect(0, 0, shirt.width, shirt.height)
+        ctx.fillStrokeShape(image)
+      }}
+    />
+    // <KonvaImage
+    //   image={image}
+    //   ref={imageRef}
+    //   filters={[Konva.Filters.Blur]}
+    //   blurRadius={10}
+    //   draggable
+    // />
   )
 }
